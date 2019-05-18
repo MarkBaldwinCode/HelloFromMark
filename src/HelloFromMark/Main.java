b7cc233b7b1e5da0e150269230c0f47e920a44cb
Matthew Norton <menorton15@hotmail.com>
2019-05-18 13:47:45 -0500

    Added a response to Mark's Hello.

--- src/HelloFromMark/Main.java
+++ src/HelloFromMark/Main.java
@@ -5,6 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello, my name is Mark.");
-        System.out.println("Hello Mark, my Name is Matthew");
+        System.out.println("Hello Mark, my name is Matthew");
     }
 }
